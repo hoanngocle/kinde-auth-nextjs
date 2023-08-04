115db507406c43c42ecdb5499c8fdf04f12be83e
hoanln <hoanln@deha-soft.com>
2023-08-04 11:49:15 +0700

    Add kinde auth

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    htmlWhitespaceSensitivity: 'css',
+    insertPragma: false,
+    bracketSameLine: false,
+    jsxSingleQuote: true,
+    printWidth: 80,
+    proseWrap: 'preserve',
+    quoteProps: 'as-needed',
+    requirePragma: false,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'none',
+    useTabs: false
+};
